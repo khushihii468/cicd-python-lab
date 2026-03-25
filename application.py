77aec36a9724ef4710a9fdc0c331ecc7003d3f11
khushihii468 <143440331+khushihii468@users.noreply.github.com>
2026-03-25 09:40:14 +0530

    Update hello_world to return HTML response

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask
 application = Flask(__name__)
 @application.route('/')
 def hello_world():
- return "CI/CD Pipeline is Live! Version 2.0: Automated
-Update Successful!"
+ return "<h1>CI/CD Pipeline is Live!</h1><p>Version 2.0: Automated
+Update Successful</p>"
 if __name__ == '__main__':
  application.run(debug=True)
